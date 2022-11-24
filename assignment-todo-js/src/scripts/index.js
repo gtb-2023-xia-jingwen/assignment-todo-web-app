774044c259d80368eef12563f19bba5e676ac431
Jingwen Xia <amyfor7@gmail.com>
2022-11-24 23:31:42 +0800

    feat: display new task

--- assignment-todo-js/src/scripts/index.js
+++ assignment-todo-js/src/scripts/index.js
@@ -3,23 +3,38 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { fetchData, postNewTask } from './api';
 import { renderCompletedList, renderTodoList } from './render';
 
+let taskList = [];
+
 fetchData().then((data) => {
-  renderTodoList(data);
-  renderCompletedList(data);
+  taskList = data;
+  renderTodoList(taskList);
+  renderCompletedList(taskList);
 });
 
 const addBtn = document.querySelector('#add-todo');
 const inputBox = document.querySelector('#todo-input');
 
-function readInputData() {
+function readAndClearInputData() {
   const res = inputBox.value;
   inputBox.value = '';
   return res;
 }
 
+function renderNewTask(data) {
+  renderTodoList(data);
+  renderCompletedList(data);
+}
+
 function addNewTask() {
-  const inputData = readInputData();
-  postNewTask(inputData);
+  const inputData = readAndClearInputData();
+  const task = {
+    name: inputData,
+    completed: false,
+  };
+  postNewTask(task).then((newTask) => {
+    taskList.push(newTask);
+    renderNewTask(taskList);
+  });
 }
 
 addBtn.addEventListener('click', addNewTask);
