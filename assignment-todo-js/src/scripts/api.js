676b5a47412eab639c6aa2a9fede5528f2334a9d
Jingwen Xia <amyfor7@gmail.com>
2022-11-24 22:36:24 +0800

    [Jingwen Xia] feat: read and post new task

--- assignment-todo-js/src/scripts/api.js
+++ assignment-todo-js/src/scripts/api.js
@@ -1,2 +1,20 @@
 const URL = 'http://localhost:8080/tasks';
 export const fetchData = () => fetch(URL).then((response) => response.json());
+
+export function postNewTask(title) {
+  const task = {
+    name: title,
+    completed: false,
+  };
+  fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
--- assignment-todo-js/src/scripts/index.js
+++ assignment-todo-js/src/scripts/index.js
@@ -1,9 +1,25 @@
 import '../styles/todo-lists.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { fetchData } from './api';
+import { fetchData, postNewTask } from './api';
 import { renderCompletedList, renderTodoList } from './render';
 
 fetchData().then((data) => {
   renderTodoList(data);
   renderCompletedList(data);
 });
+
+const addBtn = document.querySelector('#add-todo');
+const inputBox = document.querySelector('#todo-input');
+
+function readInputData() {
+  const res = inputBox.value;
+  inputBox.value = '';
+  return res;
+}
+
+function addNewTask() {
+  const inputData = readInputData();
+  postNewTask(inputData);
+}
+
+addBtn.addEventListener('click', addNewTask);
